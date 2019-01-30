4f963f95b32c610a722da2cb07189294a3e5f414
jihuayu <8042833@qq.com>
2019-01-30 08:35:25 +0800

    第1周：主类、代理和mod信息

--- src/main/java/com/example/examplemod/ExampleMod.java
+++ src/main/java/com/example/examplemod/ExampleMod.java
@@ -1,9 +1,11 @@
 package com.example.examplemod;
 
-import net.minecraft.init.Blocks;
+import com.example.examplemod.proxy.CommonProxy;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.SidedProxy;
 import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
 import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
 import org.apache.logging.log4j.Logger;
 
@@ -14,18 +16,26 @@ public class ExampleMod
     public static final String NAME = "Example Mod";
     public static final String VERSION = "1.0";
 
-    private static Logger logger;
+    @Mod.Instance(ExampleMod.MODID)
+    public static ExampleMod instance;
+
+    @SidedProxy(clientSide = "com.example.examplemod.proxy.ClientProxy",serverSide = "com.example.examplemod.proxy.CommonProxy")
+    public static CommonProxy proxy;
+
+    public static Logger logger;
 
     @EventHandler
-    public void preInit(FMLPreInitializationEvent event)
-    {
+    public void preInit(FMLPreInitializationEvent event) {
         logger = event.getModLog();
+        logger.info("pre");
     }
 
     @EventHandler
-    public void init(FMLInitializationEvent event)
-    {
-        // some example code
-        logger.info("DIRT BLOCK >> {}", Blocks.DIRT.getRegistryName());
+    public void init(FMLInitializationEvent event) {
+        logger.info("init");
+    }
+    @EventHandler
+    public void postInit(FMLPostInitializationEvent event) {
+        logger.info("post");
     }
 }
